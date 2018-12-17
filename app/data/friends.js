0e355b18f06ea6d95ae4fceebc512c929312d908
LxMO <lmorris757@outlook.com>
2018-12-17 15:25:24 -0800

    apiRoutes returning 'friends list', 'new user', and 'scores'

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+		name: "Aubrey",
+		photo: "https://imgix.bustle.com/rehost/2016/9/13/32bb3929-0c1f-462b-ab25-2852ebb87263.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+		name: "Belcalis",
+		photo: "https://listn.to/wp-content/uploads/2018/01/cardi-b-360x360.jpg",
+		scores: [1, 3, 2, 5, 1, 4, 1, 2, 3, 4]
+    },
+    {
+		name: "Elle",
+		photo: "https://d2m3klzcmjgreb.cloudfront.net/wp-content/uploads/2018/07/blonde-site-656x354.jpg",
+		scores: [1, 5, 5, 5, 4, 1, 3, 3, 3, 4]
+    },
+    {
+		name: "Josie",
+		photo: "https://andlivloves.files.wordpress.com/2017/03/tumblr_omdd72s2ix1qa8aito1_r1_540.gif?w=358",
+		scores: [4, 5, 3, 2, 1, 1, 2, 3, 4, 5]
+    },
+    {
+		name: "Betty",
+		photo: "https://a.wattpad.com/cover/148053600-352-k176739.jpg",
+		scores: [3, 2, 1, 1, 2, 3, 1, 5, 1, 4]
+    },
+    {
+		name: "Tchala",
+		photo: "https://i.pinimg.com/originals/c6/cc/22/c6cc227f3996bb86438ba96f196276a8.jpg",
+		scores: [5, 2, 5, 1, 5, 3, 5, 5, 1, 4]
+    },
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,8 +3,30 @@ var friends = require("../data/friends");
 
 //Routing//
 
-module.exports = function(app) {
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+
+    });
+
+    app.post("/api/friends", function (req, res) {
+        // console.log(req.body.scores);
+
+        var newUser = req.body;
+        // var scoreTotal = 0;
+        console.log(newUser); //New user appended to list
+        var newScoreTotal = newUser.scores.reduce(function (a, b) { return a + b; }, 0);
+        console.log(newScoreTotal);
+
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]); // a friend from the list
+            var scoreTotal = friends[i].scores.reduce(function (a, b) { return a + b; }, 0);
+            console.log(scoreTotal);
+        }
 
 
-    
-}
+    });
+
+};
+
