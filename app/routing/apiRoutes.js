5dd5f70846ce653d43b461e0a0ae868350df1b8e
LxMO <lmorris757@outlook.com>
2018-12-18 20:14:59 -0800

    final no screenshots

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,20 +12,48 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         // console.log(req.body.scores);
-
+        var scoreArr = [];
         var newUser = req.body;
-        // var scoreTotal = 0;
+ 
         console.log(newUser); //New user appended to list
-        var newScoreTotal = newUser.scores.reduce(function (a, b) { return a + b; }, 0);
+        //the 'newScoreTotal' function adds up the users choices and returns a number//
+        var newScoreTotal = newUser.scores.reduce(function (a, b) { return parseInt(a) + parseInt(b); }, 0);
         console.log(newScoreTotal);
 
         for (var i = 0; i < friends.length; i++) {
             console.log(friends[i]); // a friend from the list
             var scoreTotal = friends[i].scores.reduce(function (a, b) { return a + b; }, 0);
             console.log(scoreTotal);
-        }
+            //creates array of friendlist score totals//
 
+            if ((scoreTotal - newScoreTotal) < 0) {
+                var j = (scoreTotal - newScoreTotal) * -1;
+                scoreArr.push(j);
 
+            } else {
+                scoreArr.push(scoreTotal - newScoreTotal);
+            }
+
+        };
+        // console.log(scoreArr);
+        var smallestDiff = {
+            index: 0,
+            diff: 60
+        };
+
+        for (var i = 0; i < scoreArr.length; i++) {
+            if (scoreArr[i] <= smallestDiff.diff) {
+                smallestDiff = {
+                    index: i,
+                    diff: scoreArr[i]
+                };
+
+            };
+        };
+        console.log(friends[smallestDiff.index]);
+
+        res.json(friends[smallestDiff.index]);
+        
     });
 
 };
--- app/server.js
+++ app/server.js
@@ -12,6 +12,11 @@ var PORT = process.env.PORT || 8089;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 //routes//
 
 require("./routing/apiRoutes")(app);
